6595057de8626e691574d9200e359a7be28eccb8
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-03-11 22:58:10 +0200

    add: contacts schema timestamps

--- contacts/contactsSchema.js
+++ contacts/contactsSchema.js
@@ -1,21 +1,27 @@
 const { model, Schema } = require('mongoose');
 
-const contactsSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 const Contact = model('contacts', contactsSchema);
 
